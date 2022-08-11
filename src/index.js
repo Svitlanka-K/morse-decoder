083d8e2dbbb8ae43f85862c0b43a4db2fd5706b5
Svitlana <93516763+Svitlanka-K@users.noreply.github.com>
2022-08-11 23:01:57 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,21 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+     let arr = expr.match(/.{1,10}/g); 
+       
+  let x = [];
+    arr.forEach(function(arr) {
+    x.push(arr.replaceAll('10', "."))
+  })
+    
+    let y = [];
+    x.forEach(function(x) {
+    y.push(x.replaceAll('11', "-"))
+  })
+      
+    let str = y.join(' ');
+      
+  return str.split('**********').map(x => x.split(' ').map(a => a.split('0').map(b => MORSE_TABLE[b]).join('')).join('')).join(' ')
 }
 
 module.exports = {
